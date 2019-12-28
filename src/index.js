8bddb34f55b2bdeaff8e1533eb8a5b6b15f91aef
Juan Collado <juancollado.dev@gmail.com>
2019-12-28 15:08:29 -0300

    Create endpoints to get Users and Task
    By id and all of them

--- src/index.js
+++ src/index.js
@@ -18,6 +18,23 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req,res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(404).send('User not found')
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -25,6 +42,24 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req,res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req,res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(404).send('Task not found')
     })
 })
 
