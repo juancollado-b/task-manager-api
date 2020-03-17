8ae80d4acbf6eba9aec874663d916ce384c5d51b
Juan Collado <juancollado.dev@gmail.com>
2020-03-17 00:19:15 -0300

    Install Bcrypt and start hashing password when user is created or edited

--- src/models/users.js
+++ src/models/users.js
@@ -1,44 +1,57 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require("bcryptjs")
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
         trim: true,
-        required: true
-
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
         type: Number,
         default: 0,
         validate(value) {
             if (value < 0) {
-                throw new Error('The number have to be positive')
-            }
-        }
-    },
-    mail: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        required: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Invalid mail')
-            }
-        }
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-        minlength: 7,
-        validate(value) {
-            if (value.includes('password')) {
-                throw new Error(`The password can't contain the word 'password' lol`)
+                throw new Error('Age must be a postive number')
             }
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
