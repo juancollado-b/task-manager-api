b9bd5af234c226b52dd50dc66f0db6e4130bb3ba
Juan Collado <juancollado.dev@gmail.com>
2020-03-17 18:01:06 -0300

    Create a new static for userSchema for check if mail/password are correct

--- src/models/users.js
+++ src/models/users.js
@@ -42,6 +42,22 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Email or password incorect')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Email or password incorect')
+    }
+    
+    return user
+}
+
 userSchema.pre('save', async function (next) {
     const user = this
 
